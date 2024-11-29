597aa9199569b913ca29eaf3611bc8f8a601046a
Mihail Zakharov <Mihail.Zaharov1@lemanapro.ru>
2024-11-29 11:11:49 +0400

    My unit tests from Sprint3

---
+++ task_3.java
@@ -0,0 +1,32 @@
+Параметризация с проверкой возраста на тру
+@RunWith(Parameterized.class) // Напиши аннотацию для параметризованных тестов
+public class CheckIsAdultTest {
+	
+  private final int age;
+  private final boolean result;
+
+  public CheckIsAdultTest(int age, boolean result) {
+	this.age = age;
+    this.result = result;// Инициализируй поля класса в конструкторе
+  }
+
+  @Parameterized.Parameters // Пометь метод аннотацией для параметров
+  public static Object[][] getTextData() {
+	return new Object[][] {
+        {18, true},
+        {19, true},
+        {22, true},
+        {21, true},
+        // Заполни массив тестовыми данными и ожидаемым результатом
+    };
+  }
+
+  @Test
+  public void checkIsAdultWhenAgeThenResult() {
+	Program program = new Program();
+     int age = 18;// Передай сюда возраст пользователя
+    boolean isAdult = program.checkIsAdult(age);
+	// Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+    assertEquals("Тру если возраст более 18", result, isAdult);
+	}
+}
